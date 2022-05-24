e8aa981dae3f5f9dbecc4f2072a41684890bf006
Thiago Saraiva <saraii_@live.com>
2022-05-24 09:47:58 -0300

    Add files via upload

---
+++ screens/IssLocation.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import {
+    Text,
+    View,
+    StyleSheet,
+    ImageBackground,
+    StatusBar,
+    SafeAreaView,
+    Alert,
+    Platform
+} from 'react-native';
+
+export default class IssLocationScreen extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <SafeAreaView style={styles.droidSafeArea} />
+                <ImageBackground source={require('../assets/bg.png')} style={styles.backgroundImage}>
+                    <View style={styles.titleContainer}>
+                        <Text style={styles.titleText}>Localização da EEI</Text>
+                    </View>
+                </ImageBackground>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    droidSafeArea: {
+        marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+    },
+    backgroundImage: {
+        flex: 1,
+        resizeMode: 'cover',
+    },
+    titleContainer: {
+        flex: 0.1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    titleText: {
+        fontSize: 30,
+        fontWeight: "bold",
+        color: "white"
+    },
+})
